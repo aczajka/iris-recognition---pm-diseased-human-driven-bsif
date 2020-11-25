c3533731c5dd17fbea3345eb58ea0b1a085aba21
Adam Czajka <aczajka@nd.edu>
2020-11-25 14:51:03 -0500

    cartesian->polar fix by Adam

--- matlab/pmIrisCartesianToPolar.m
+++ matlab/pmIrisCartesianToPolar.m
@@ -6,6 +6,8 @@ POL_W = length(pupilData.theta);
 IMAGE_POL = uint8(zeros(POL_H,POL_W));
 MASK_POL = uint8(zeros(POL_H,POL_W));
 
+[imH,imW,~] = size(IMAGE);
+
 for j = 1:POL_W
     
     for i = 1:POL_H
@@ -13,14 +15,11 @@ for j = 1:POL_W
         radius = i / POL_H;
         x = round((1-radius) * pupilData.xCirclePoints(j) + radius * irisData.xCirclePoints(j));
         y = round((1-radius) * pupilData.yCirclePoints(j) + radius * irisData.yCirclePoints(j));
-
-        % make sure the coordinates are not outside the image
+        
         if (x > 0 && x <= imW && y > 0 && y <= imH)
             IMAGE_POL(i,j) = IMAGE(y,x);
             MASK_POL(i,j) = 255*MASK(y,x);
         end
-
-        IMAGE_POL(i,j) = IMAGE(y,x);
-        MASK_POL(i,j) = 255*MASK(y,x);
+        
     end
 end
